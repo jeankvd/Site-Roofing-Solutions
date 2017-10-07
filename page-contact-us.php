2f06d62a5eabd9d4489072cd42666ec6c56c088d
jeankvd <djjean85@gmail.com>
2017-10-06 23:48:54 -0400

    "Added styles to contact page"

--- page-contact-us.php
+++ page-contact-us.php
@@ -1,31 +1,69 @@
 <?php get_header(); ?>
+
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post();?>
-<div class="jumbotron">
+
+<div class="jumbotron" id="contact-banner">
+  <div id="overlay"></div>
   <h1><?php the_title(); ?></h1>
 </div>
-<!-- Banne -->
-Some Banner
 
 <!-- Content -->
 <div class="container">
   <hr>
-  <h2 id="info"><?php the_content(); ?></h2>
+  <p id="contact-content">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.<?php the_content(); ?></p>
   <hr>
+  <div id="contact-info">
+    <p>3203 e 6ht st <br> Miami, Florida 33972, USA</p>
+    <p>(239)-200-2373</p>
+  </div>
 </div>
 
 <?php endwhile; ?>
 <?php endif; ?>
 
+
+
+<hr>
+
 <!-- Form -->
 <div class="container">
-  <form class="row">
-     <input class="col-6" type="text" name="first_name" id="first_name" placeholder="first_Name">
-     <input class="col-6 ml-auto" type="text" name="last_name" id="last_name" placeholder="last_Name">
-     <input class="col-6" type="email" name="email" id="email" placeholder="Email">
-     <input class="col-6 ml-auto" type="tel" name="phone" placeholder="Phone">
-     <textarea placeholder="How can We Help" class="col-12"></textarea>
-     <input type="submit" name="submit" action="post" value="Submit">
+  <form id="main">
+
+<h2>Contact Us</h2>
+
+<h4>Name</h4>
+  <div class="row">
+    <div class="col-6">
+      <input class="" type="text" name="first_name" id="first_name" placeholder="">
+      <label for="first_name">First.</label>
+    </div>
+
+    <div class="col-6">
+      <input class="" type="text" name="last_name" id="last_name" placeholder="">
+      <label for="last_Name">Last.</label>
+    </div>
+  </div>
+
+  <div class="row">
+    <div class="col-6">
+      <h4>Email</h4>
+      <input class="" type="email" name="email" id="email" placeholder="">
+    </div>
+
+  <div class="col-6">
+    <h4>Phone</h4>
+    <input class="col-6 ml-auto" type="tel" name="phone" placeholder="">
+  </div>
+  </div>
+
+
+
+    <h4>How can we help You</h4>
+    <textarea placeholder="" class="col-12"></textarea>
+
+    <input type="submit" name="submit" action="post" value="Submit">
   </form>
 </div>
+
 
 <?php get_footer( );?>
